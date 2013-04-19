9ae1e8c309b7f9290d7be7ced8c4e01c5c784311
Henri Bergius <henri.bergius@iki.fi>
2013-04-19 14:05:02 +0200

    Revert mistaken merge

--- components.js
+++
@@ -1,8 +0,0 @@
-// Manual registration of components to make available for the ComponentLoader
-//
-// This is an interim solution until https://github.com/component/builder.js/pull/62#issuecomment-16296342 is implemented
-exports.register = function (loader) {
-  loader.registerComponent('', 'Split', '/noflo/components/Split.js');
-  loader.registerComponent('', 'Merge', '/noflo/components/Merge.js');
-  loader.registerComponent('', 'Callback', '/noflo/components/Callback.js');
-};
